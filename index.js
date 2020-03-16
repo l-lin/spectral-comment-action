9c5313093644af8abc0171cc883df0a3ace92284
Louis Lin <lin.louis@pm.me>
2020-03-16 09:36:52 +0100

    chore: adapt project

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const wait = require('./wait');
 
 // most @actions toolkit packages have async methods
 async function run() {
-  try { 
+  try {
     const ms = core.getInput('milliseconds');
     console.log(`Waiting ${ms} milliseconds ...`)
 
@@ -13,7 +13,7 @@ async function run() {
     core.debug((new Date()).toTimeString())
 
     core.setOutput('time', new Date().toTimeString());
-  } 
+  }
   catch (error) {
     core.setFailed(error.message);
   }
